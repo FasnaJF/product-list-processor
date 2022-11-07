b53bbb2dd6271d82b348cce079b2d48d8a9fce8f
jffasna <fasna@rhino-partners.com>
2022-11-08 00:41:46 +0530

    added formatted input files

--- parser.php
+++ parser.php
@@ -1,3 +1,12 @@
 <?php
 
 include_once $_SERVER['PWD'] . '/autoload.php';
+
+use src\ProductListProcessor;
+
+$processor = new ProductListProcessor();
+try {
+    $processor->initiate($argv);
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage() . "\n";
+}
